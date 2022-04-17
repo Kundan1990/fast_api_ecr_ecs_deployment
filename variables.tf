19e82c3be8a0d4b06cbdba3b29b98ef665aa8267
Kundan1990 <103748913+Kundan1990@users.noreply.github.com>
2022-04-18 00:25:09 +0530

    updated variables.tf

--- variables.tf
+++ variables.tf
@@ -4,3 +4,4 @@ variable "project" { default = "fast-api-ecs-depl" }
 variable "platform" { default = "Challenge" }
 variable "vpc_id" { default = "vpc-0fe8f9826861ebbe5" }
 variable "team" { default = "kundan-team" }
+variable "img" { default = "127215006084.dkr.ecr.us-east-1.amazonaws.com/fast_api_app:latest" } 
